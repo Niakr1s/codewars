c195dba8cfc5a64a7f736e6c123ea1ae64277eb1
Niakr1s <pavel2188@gmail.com>
2020-06-29 21:57:57 +0300

    changed ConvertStringToCamelCase to use StringBuilder

--- java/ConvertStringToCamelCase/src/Solution.java
+++ java/ConvertStringToCamelCase/src/Solution.java
@@ -1,11 +1,14 @@
-class Solution{
-    static String toCamelCase(String s){
+class Solution {
+    static String toCamelCase(String s) {
         String[] subStrings = s.split("[^a-zA-Z]");
+        if (subStrings.length == 0) return "";
+        StringBuilder builder = new StringBuilder();
+        builder.append(subStrings[0]);
         // converting all substrings to uppercase, skipping first substring
         for (int i = 1; i < subStrings.length; i++) {
             String str = subStrings[i];
-            subStrings[i] = Character.toUpperCase(str.charAt(0)) + str.substring(1);
+            builder.append(Character.toUpperCase(str.charAt(0))).append(str.substring(1));
         }
-        return String.join("", subStrings);
+        return builder.toString();
     }
 }
